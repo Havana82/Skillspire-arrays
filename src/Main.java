848f80235421a6b5ce001fa2ee87b44c4789ebe3
halimAremu <halimat3000@gmail.com>
2024-09-07 12:01:04 -0500

    add code

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=10; i>0; i--){
+            System.out.println(i);
+        }
+        System.out.println("Done");
+        String[] students = {"Cate", "Mark", "Anthony", "Jacob", "Robert"};
+        double[] grades = {5.0, 2.5, 4.2, 4.0, 5.0};
+        System.out.println(getStudentGrades(2, students, grades));
+    }
+
+    public static String getStudentGrades(int ind, String[] student, double[] grades){
+        return "Student:"+ student[ind] + "Grade:" + grades[ind];
+    }
+
+
+}
